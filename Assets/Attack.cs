5c4f16065819a3457685132312e6ab8227a8f3fb
stabanazr <stabanazr@gmail.com>
2023-12-17 22:34:23 -0800

    Melee Hitbox
    Generating placeholder melee hitbox.

---
+++ Assets/Attack.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Build;
+using UnityEngine.InputSystem;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    public GameObject Melee;
+    bool isAttacking = false;
+    float atkDuration = 0.3f;
+    float atkTimer = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckMeleeTimer();
+    }
+
+    public void OnAttack(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            if (!isAttacking)
+            {
+                Melee.SetActive(true);
+                isAttacking = true;
+                // Call animator to play melee attack here
+            }
+        }
+    }
+
+    void CheckMeleeTimer()
+    {
+        if (isAttacking)
+        {
+            atkTimer += Time.deltaTime;
+            if (atkTimer >= atkDuration)
+            {
+                atkTimer = 0;
+                isAttacking = false;
+                Melee.SetActive(false);
+            }
+        }
+    }
+}
