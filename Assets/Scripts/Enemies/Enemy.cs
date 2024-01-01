e28b66c9cf106fc09e6f8432ccae806f3ea9563f
stabanazr <stabanazr@gmail.com>
2024-01-01 01:00:44 -0800

    Added basic aggro and leashing
    Basic orc enemy will now pursue player when within aggro range, and return to waypoint when out of range.

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -5,24 +5,49 @@ using UnityEngine;
 public abstract class Enemy : MonoBehaviour
 {
     Rigidbody rb;
-    [SerializeField]
-    protected float health;
-    [SerializeField]
-    protected float maxHealth;
-    [SerializeField]
-    protected float speed;
-    [SerializeField]
-    protected float range;
+    [SerializeField] protected float health;
+    [SerializeField] protected float maxHealth;
+    [SerializeField] protected float speed;
+    [SerializeField] protected float attackRange;
+    [SerializeField] protected float aggroRange;
+    protected Transform target;
+    [SerializeField] protected Vector3 playerDirection; // Can remove serialize after testing
+    protected Vector3 patrolDirection;
+    public bool canMove = true;
+
     // Start is called before the first frame update
     protected virtual void Start()
     {
         health = maxHealth;
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    protected virtual void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     protected virtual void Update()
     {
-        
+        if (canMove)
+        {
+            Move();
+        }
+    }
+
+    protected virtual void Move()
+    {
+        // Check for location of player relative to enemy
+        playerDirection = (target.position - transform.position);
+        // Set y to zero to prevent vertical movement
+        playerDirection.y = 0;
+        if (playerDirection.magnitude <= aggroRange)
+        {
+            playerDirection = playerDirection.normalized;
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(playerDirection), 0.15f);
+            transform.position = Vector3.MoveTowards(transform.position, new Vector3(target.position.x - playerDirection.x * attackRange, 0, target.position.z - playerDirection.z * attackRange), speed * Time.deltaTime);
+        }
     }
 
     public virtual void TakeDamage(float damage)
--- Assets/Scripts/Enemies/Orc.cs
+++ Assets/Scripts/Enemies/Orc.cs
@@ -4,5 +4,26 @@ using UnityEngine;
 
 public class Orc : Enemy
 {
-    
+    private Transform waypointTarget;
+    public Transform waypoint1; // alternate target between multiple waypoints to create patrolling enemies 
+    protected override void Awake()
+    {
+        base.Awake();
+        waypointTarget = waypoint1;
+    }
+    protected override void Move()
+    {
+        base.Move();
+        if (playerDirection.magnitude > aggroRange)
+        {
+            patrolDirection = (waypointTarget.position - transform.position);
+            // Set y to zero to prevent vertical movement
+            patrolDirection.y = 0;
+            if (patrolDirection.magnitude > 0.1)
+            {
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(patrolDirection), 0.15f);
+                transform.position = Vector3.MoveTowards(transform.position, new Vector3(waypointTarget.position.x, 0, waypointTarget.position.z), speed * Time.deltaTime);
+            }
+        }
+    }
 }
