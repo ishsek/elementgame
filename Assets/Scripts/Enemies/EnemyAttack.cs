b439051605ffd8dbee4fe826d750761dc0401ae8
stabanazr <stabanazr@gmail.com>
2024-01-14 21:37:41 -0800

    Added cooldown to enemy attack

--- Assets/Scripts/Enemies/EnemyAttack.cs
+++ Assets/Scripts/Enemies/EnemyAttack.cs
@@ -11,6 +11,8 @@ public class EnemyAttack : MonoBehaviour
     public float atkDuration = 0.3f;
     float atkTimer = 0f;
     public float atkDelay = 0.3f;
+    public float cooldown = 1f; //seconds
+    private float lastAttackedAt = -9999f;
 
     // Update is called once per frame
     void Update()
@@ -32,22 +34,30 @@ public class EnemyAttack : MonoBehaviour
 
     void CheckMeleeTimer()
     {
+
         if (isAttacking)
         {
-            atkTimer += Time.deltaTime;
-            Enemy attacker = GetComponent<Enemy>();
-            // launch actual attack
-            if (atkTimer >= atkDuration + atkDelay)
+            // Check attack cooldown
+            if (Time.time > lastAttackedAt + cooldown)
             {
-                atkTimer = 0;
-                isAttacking = false;
-                Melee.SetActive(false);
-                attacker.Mobilize();
-            }
-            // attack delay
-            else if (atkTimer >= atkDelay)
-            {
-                Melee.SetActive(true);
+                // Start attack process
+                atkTimer += Time.deltaTime;
+                // end attack if delay + attack time has expired
+                if (atkTimer >= atkDuration + atkDelay)
+                {
+                    atkTimer = 0;
+                    isAttacking = false;
+                    Melee.SetActive(false);
+                    Enemy attacker = GetComponent<Enemy>();
+                    attacker.Mobilize();
+                    // Set timer for attack cooldown
+                    lastAttackedAt = Time.time;
+                }
+                // start attack if delay period has expired
+                else if (atkTimer >= atkDelay)
+                {
+                    Melee.SetActive(true);
+                }
             }
         }
     }
