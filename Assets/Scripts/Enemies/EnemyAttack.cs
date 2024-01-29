ad9e2257768f9401546b7a8db5bee6cc18ef01c5
stabanazr <stabanazr@gmail.com>
2024-01-28 16:39:29 -0800

    Fixed enemy attack cooldown bug
    Enemy animation is now affected by cooldown

--- Assets/Scripts/Enemies/EnemyAttack.cs
+++ Assets/Scripts/Enemies/EnemyAttack.cs
@@ -14,7 +14,7 @@ public class EnemyAttack : MonoBehaviour
     float atkTimer = 0f;
     public float atkDelay = 0.3f;
     public float cooldown = 1f; //seconds
-    private float lastAttackedAt = -9999f;
+    private float cdTimer = 9999f;
 
     public Enemy Attacker;
 
@@ -22,21 +22,26 @@ public class EnemyAttack : MonoBehaviour
     void Update()
     {
         CheckMeleeTimer();
+        cdTimer += Time.deltaTime;
     }
 
     public void OnAttack()
     {
         if (!isAttacking)
         {
-            //Melee.SetActive(true);
-            isAttacking = true;
-            // Call animator to play melee attack here
-            if (EnemyAnimator != null)
+            // Check cd
+            if (cdTimer > cooldown)
             {
-                EnemyAnimator.SetTrigger(AnimationTriggersStatic.GetEnemyAttackTrigger());
-            }
+                //Melee.SetActive(true);
+                isAttacking = true;
+                // Call animator to play melee attack here
+                if (EnemyAnimator != null)
+                {
+                    EnemyAnimator.SetTrigger(AnimationTriggersStatic.GetEnemyAttackTrigger());
+                }
 
-            Attacker.Immobilize();
+                Attacker.Immobilize();
+            }
         }
     }
 
@@ -44,26 +49,22 @@ public class EnemyAttack : MonoBehaviour
     {
         if (isAttacking)
         {
-            // Check attack cooldown
-            if (Time.time > lastAttackedAt + cooldown)
+            // Start attack process
+            atkTimer += Time.deltaTime;
+            // end attack if delay + attack time has expired
+            if (atkTimer >= atkDuration + atkDelay)
             {
-                // Start attack process
-                atkTimer += Time.deltaTime;
-                // end attack if delay + attack time has expired
-                if (atkTimer >= atkDuration + atkDelay)
-                {
-                    atkTimer = 0;
-                    isAttacking = false;
-                    Melee.SetActive(false);
-                    Attacker.Mobilize();
-                    // Set timer for attack cooldown
-                    lastAttackedAt = Time.time;
-                }
-                // start attack if delay period has expired
-                else if (atkTimer >= atkDelay)
-                {
-                    Melee.SetActive(true);
-                }
+                atkTimer = 0;
+                isAttacking = false;
+                Melee.SetActive(false);
+                Attacker.Mobilize();
+                // Set timer for attack cooldown
+                cdTimer = 0;
+            }
+            // start attack if delay period has expired
+            else if (atkTimer >= atkDelay)
+            {
+                Melee.SetActive(true);
             }
         }
     }
